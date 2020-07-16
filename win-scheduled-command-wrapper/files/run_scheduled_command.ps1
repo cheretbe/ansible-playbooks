1598fcebed3c1cdca11691a355ce64f63add5332
cheretbe <a.a.orlov@gmail.com>
2020-07-16 22:46:47 +0200

    win-scheduled-command-wrapper: Use jinja template for wrapper script

--- win-scheduled-command-wrapper/files/run_scheduled_command.ps1
+++
@@ -1,7 +0,0 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
-
-Write-Output "There you go"
-"There you go" | Out-File -Append -FilePath "c:\test.txt"
-
-throw "test exception"
