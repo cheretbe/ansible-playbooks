268a61cd6ac95aa7d4e78d2f251eac657b60651c
cheretbe <a.a.orlov@gmail.com>
2021-03-28 10:17:29 +0200

    tests: add linux server deployment test

---
+++ tests/deployment/test_linux_server_deployment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+(
+  cd "${script_path}/../.."
+
+  if [ -z "$1" ]; then
+    hosts_param=""
+  else
+    hosts_param="--hosts=${1}"
+  fi
+
+  py.test -v --connection=ansible ${hosts_param}\
+    linux-utils/tests \
+    linux-dns/tests \
+    linux-locale/tests \
+    linux-mta/tests \
+    linux-timezone/tests \
+    linux-unattended-upgrades/tests
+)
