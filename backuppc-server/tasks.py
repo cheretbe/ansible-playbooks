8d07705aca81e1b35e1420155b702da0ec7eaa1b
cheretbe <a.a.orlov@gmail.com>
2021-04-28 16:42:58 +0200

    backuppc-server: skip some Molecule scenarios during full testing to reduce overall time

--- backuppc-server/tasks.py
+++ backuppc-server/tasks.py
@@ -26,11 +26,15 @@ def show_help(context):
 @invoke.task
 def test(context):
     """Run all tests"""
-    for scenario in test_utils.get_molecule_scenarios(context):
+    scenarios = test_utils.get_molecule_scenarios(context)
+    filtered = ["backuppc", "backuppc-xs", "rsync-bpc", "apache"]
+    scenarios = [scenario for scenario in scenarios if scenario not in filtered]
+    for scenario in scenarios:
         for driver in ["docker", "lxd"]:
+            platform = "linux" if scenario == "failing" else "ubuntu"
             header_text = f"Molecule test {scenario} ({driver})"
             test_utils.print_header(header_text)
-            test_utils.run_molecule(context, "test", scenario, driver)
+            test_utils.run_molecule(context, "test", scenario, driver, platform)
             test_utils.print_success_message(header_text)
 
 
@@ -39,4 +43,5 @@ def molecule(
         context, command, scenario=None, driver="docker"
 ):
     """Run custom Molecule command"""
-    test_utils.run_molecule(context, command, scenario, driver, platform="ubuntu")
+    platform = "linux" if scenario == "failing" else "ubuntu"
+    test_utils.run_molecule(context, command, scenario, driver, platform=platform)
