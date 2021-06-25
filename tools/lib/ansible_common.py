d445f3a15f8262415d1302204c4140515daa9b1f
cheretbe <a.a.orlov@gmail.com>
2021-06-25 09:40:59 +0200

    Tools: make env variable hint more prominent by using color

--- tools/lib/ansible_common.py
+++ tools/lib/ansible_common.py
@@ -1,10 +1,15 @@
 import os
 import subprocess
-import tempfile
-import threading
 import pathlib
+import colorama
 
 script_dir = pathlib.Path(__file__).resolve().parent
+
+colorama.init()
+
+def color_print(fore_color, msg):
+    # print("{color}{msg}{reset}".format(color=fore_color, msg=msg, reset=colorama.Style.RESET_ALL))
+    print(f"{fore_color}{msg}{colorama.Style.RESET_ALL}")
 
 def run(cmd_args, echo=True, **kwargs):
     if echo:
@@ -21,6 +26,10 @@ def run_ansible_with_vault(ansible_args):
             str(script_dir / "ansible_vault_env_pass_file.sh")
         ]
     else:
+        color_print(
+            colorama.Fore.CYAN + colorama.Style.BRIGHT,
+            "Hint: consider setting ANSIBLE_VAULT_PASSWORD environment variable"
+        )
         ansible_cmd += ["--ask-vault-password"]
 
     run(ansible_cmd + ansible_args)
